160282cf02791cf02001c6f87b0f4fe20e51aa3f
markevich <slava.markevich@gmail.com>
2018-12-27 20:49:07 +0700

    closes #14: Update CO2 emission from plane

--- test/co2_offset/converters/plane_test.exs
+++ test/co2_offset/converters/plane_test.exs
@@ -5,31 +5,31 @@ defmodule Co2Offset.Converters.PlaneTest do
 
   describe "convert" do
     test "co2_from_km returns a correct amount of co2 for 1 km" do
-      assert(0.18 = Plane.convert(1.0, :co2_from_km))
+      assert(0.36 = Plane.convert(1.0, :co2_from_km))
     end
 
     test "co2_from_km/1 returns a correct amount of co2 for 3km" do
-      assert(0.54 = Plane.convert(3.0, :co2_from_km))
+      assert(1.08 = Plane.convert(3.0, :co2_from_km))
     end
 
     test "km_from_co2/1 returns a correct amount of km for 0.18 co2" do
-      assert(1.0 = Plane.convert(0.18, :km_from_co2))
+      assert(1.0 = Plane.convert(0.36, :km_from_co2))
     end
 
     test "km_from_co2/1 returns a correct amount of km for 0.54 co2" do
-      assert(3.0 = Plane.convert(0.54, :km_from_co2))
+      assert(3.0 = Plane.convert(1.08, :km_from_co2))
     end
   end
 
   describe "convert_and_structure" do
     test "returns correct struct" do
-      co2 = 0.18
+      co2 = 0.36
 
       expected = {
         co2,
         [
           %{
-            co2: 0.18,
+            co2: co2,
             km: 1,
             type: :plane
           }
