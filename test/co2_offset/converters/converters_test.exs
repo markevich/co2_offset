b145a8098e5badf5bbcdc7f7838681c04c859ff9
markevich <slava.markevich@gmail.com>
2018-12-27 15:55:27 +0700

    closes #7: Add plane converter

---
+++ test/co2_offset/converters/converters_test.exs
@@ -0,0 +1,24 @@
+defmodule Co2Offset.ConvertersTest do
+  use Co2Offset.DataCase
+
+  alias Co2Offset.Converters
+
+  describe "converters" do
+    test "from_plane/1 returns a correct mapping" do
+      km = 1.0
+
+      expected = {
+        0.18,
+        [
+            %{
+            co2: 0.18,
+            km: 1.0,
+            type: :plane
+          }
+        ]
+      }
+
+      assert(Converters.from_plane(km) == expected)
+    end
+  end
+end
