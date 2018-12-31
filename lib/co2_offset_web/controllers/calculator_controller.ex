69dabfc66915173fe2fa4014eae1b52817b51c12
markevich <slava.markevich@gmail.com>
2018-12-31 20:31:33 +0700

    closes #31: Add basic html markup

--- lib/co2_offset_web/controllers/calculator_controller.ex
+++ lib/co2_offset_web/controllers/calculator_controller.ex
@@ -1,7 +1,11 @@
 defmodule Co2OffsetWeb.CalculatorController do
   use Co2OffsetWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  alias Co2Offset.Converters
+
+  def show(conn, _params) do
+    calculator = Converters.from_plane(700.0)
+
+    render(conn, "show.html", calculator: calculator)
   end
 end
--- lib/co2_offset_web/views/calculator_view.ex
+++ lib/co2_offset_web/views/calculator_view.ex
@@ -1,3 +1,14 @@
 defmodule Co2OffsetWeb.CalculatorView do
   use Co2OffsetWeb, :view
+
+  def render("show.html", %{calculator: calculator}) do
+    {co2_amount, data} = calculator
+
+    render_template("show.html", co2_amount: co2_amount, data: data)
+  end
+
+  def float_to_string(float) do
+    float
+    |> :erlang.float_to_binary([:compact, {:decimals, 2}])
+  end
 end
