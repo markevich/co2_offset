da5d64c0436f7a6820991d7651a595768bacec85
markevich <slava.markevich@gmail.com>
2019-02-08 12:13:20 +0700

    resolve #40: Add test for calculator view

--- test/co2_offset_web/views/calculator_view_test.exs
+++ test/co2_offset_web/views/calculator_view_test.exs
@@ -1,3 +1,9 @@
 defmodule Co2OffsetWeb.CalculatorViewTest do
   use Co2OffsetWeb.ConnCase, async: true
+
+  alias Co2OffsetWeb.CalculatorView
+
+  test "float_to_string/1" do
+    assert CalculatorView.float_to_string(6.666) == "6.67"
+  end
 end
