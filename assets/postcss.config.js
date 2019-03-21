73bad62cb5bf50600b241716b2d3c57b505b3bd6
markevich <slava.markevich@gmail.com>
2019-03-21 13:31:16 +0800

    resolves #69: Add and configure PostCss

---
+++ assets/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "postcss-plugin": {},
+    "postcss-custom-properties": {},
+    "autoprefixer": {},
+    "postcss-reporter": {}
+  }
+}
