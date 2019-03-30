6d673f35083fe98a32ff6d49c74d9921589645f1
markevich <slava.markevich@gmail.com>
2019-03-30 12:27:02 +0800

    relates #67. Implement new flights page prototype

--- lib/co2_offset_web/controllers/flight_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule Co2OffsetWeb.FlightController do
-  use Co2OffsetWeb, :controller
-
-  def new(conn, _params) do
-    render(conn, "new.html")
-  end
-end
--- lib/co2_offset_web/live/flight_live/new.ex
+++ lib/co2_offset_web/live/flight_live/new.ex
@@ -1,30 +1,22 @@
 defmodule Co2OffsetWeb.FlightLive.New do
   use Phoenix.LiveView
-  import Calendar.Strftime
+
+  @moduledoc """
+  Live view for Flights/new page.
+  Dynamic content:
+    flight_from, flight_to inputs
+    form validation and submition
+  """
 
   def render(assigns) do
-    ~L"""
-    <div>
-      <h2 phx-click="boom">It's <%= strftime!(@date, "%r") %></h2>
-    </div>
-    """
+    Co2OffsetWeb.FlightView.render("new.html", assigns)
   end
 
   def mount(_session, socket) do
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
-
-    {:ok, put_date(socket)}
-  end
-
-  def handle_info(:tick, socket) do
-    {:noreply, put_date(socket)}
+    {:ok, socket}
   end
 
   def handle_event("nav", _path, socket) do
     {:noreply, socket}
-  end
-
-  defp put_date(socket) do
-    assign(socket, date: :calendar.local_time())
   end
 end
