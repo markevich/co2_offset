83381d037e521f9693bd719e959dcdbd2969d9b2
markevich <slava.markevich@gmail.com>
2019-04-08 13:29:39 +0700

    resolve #74: Add SSL to both dev and prod

--- rel/sys/test_release_env.sh
+++ rel/sys/test_release_env.sh
@@ -6,3 +6,6 @@ export DATABASE_NAME=co2_offset_dev
 export PORT=4001
 export ERLANG_COOKIE=KLJFDKSHF*@HFSUGDF#$JDSSD*SDF#HD*DF%$ASGJBMXMCBNN{}
 export LIVE_VIEW_SALT=mix_phx.gen.secret_32
+export CO2_OFFSET_SSL_KEY_PATH=/etc/letsencrypt/live/domain_name/privkey.pem
+export CO2_OFFSET_SSL_CERT_PATH=/etc/letsencrypt/live/domain_name/chain.pem
+export CO2_OFFSET_SSL_CACERT_PATH=/etc/letsencrypt/live/domain_name/cert.pem
