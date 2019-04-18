83e218c1f5e682d8368aec0f26c164e066f51749
markevich <slava.markevich@gmail.com>
2019-04-19 00:47:54 +0800

    relates #93: Release 0.3.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Co2Offset.MixProject do
   def project do
     [
       app: :co2_offset,
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.8.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
