c786a613dd3d105fb42744d7adc472dfef2082bd
markevich <slava.markevich@gmail.com>
2019-04-23 12:51:41 +0800

    relates #92: Round required values

--- lib/co2_offset_web/views/calculator_view.ex
+++ lib/co2_offset_web/views/calculator_view.ex
@@ -1,8 +1,12 @@
 defmodule Co2OffsetWeb.CalculatorView do
   use Co2OffsetWeb, :view
 
-  def float_to_string(float) do
+  def to_float_string(float) do
     float
     |> :erlang.float_to_binary([:compact, {:decimals, 2}])
+  end
+
+  def to_int(float) do
+    round(float)
   end
 end
