5060ec635cc9e9ca68486c6135f188d90b04f3c3
markevich <slava.markevich@gmail.com>
2019-04-24 23:08:36 +0800

    relates #87: Implement autocomplete prototype

--- lib/co2_offset_web/live/calculator_live/new.ex
+++ lib/co2_offset_web/live/calculator_live/new.ex
@@ -2,8 +2,12 @@ defmodule Co2OffsetWeb.CalculatorLive.New do
   use Phoenix.LiveView
   alias Co2Offset.Calculators
   alias Co2Offset.Calculators.Calculator
+  alias Co2Offset.Geo.Airport
   alias Co2OffsetWeb.CalculatorLive.Show
   alias Co2OffsetWeb.Router.Helpers, as: Routes
+
+  import Ecto.Query
+  alias Co2Offset.Repo
 
   @moduledoc """
   Live view for Flights/new page.
@@ -19,21 +23,58 @@ defmodule Co2OffsetWeb.CalculatorLive.New do
   def mount(_session, socket) do
     {:ok,
      assign(socket, %{
-       changeset: Calculators.change_calculator(%Calculator{})
+       changeset: Calculators.change_calculator(%Calculator{}),
+       show_iata_from_autocomplete: false,
+       show_iata_to_autocomplete: false,
+       iata_from_autocomplete: %{},
+       iata_to_autocomplete: %{}
      })}
   end
 
-  # def handle_event("validate", %{"calculator" => params}, socket) do
-  #   IO.puts("OASDKLASJDALSKJDALSKDJ")
-  #   changeset =
-  #     %Calculator{}
-  #     |> Co2Offset.Calculators.change_calculator(params)
-  #     |> Map.put(:action, :insert)
+  def handle_event(
+        "autocomplete",
+        %{"calculator" => %{"iata_from" => iata_from, "iata_to" => iata_to}},
+        socket
+      ) do
+    iata_from_autocomplete = get_autocomplete_records(iata_from)
+    iata_to_autocomplete = get_autocomplete_records(iata_to)
 
-  #   IO.puts(params)
-  #   IO.puts(changeset)
-  #   {:noreply, assign(socket, changeset: changeset)}
-  # end
+    {:noreply,
+     assign(socket,
+       iata_from_autocomplete: iata_from_autocomplete,
+       iata_to_autocomplete: iata_to_autocomplete
+     )}
+  end
+
+  def handle_event(
+        "set_iata_from",
+        value,
+        %Phoenix.LiveView.Socket{assigns: %{changeset: changeset}} = socket
+      ) do
+    new_changeset = Ecto.Changeset.put_change(changeset, :iata_from, value)
+
+    {:noreply, assign(socket, %{changeset: new_changeset, show_iata_from_autocomplete: false})}
+  end
+
+  def handle_event(
+        "set_iata_to",
+        value,
+        %Phoenix.LiveView.Socket{assigns: %{changeset: changeset}} = socket
+      ) do
+    new_changeset = Ecto.Changeset.put_change(changeset, :iata_to, value)
+
+    {:noreply, assign(socket, changeset: new_changeset, show_iata_to_autocomplete: false)}
+  end
+
+  def handle_event("show_iata_from_autocomplete", _params, socket) do
+    {:noreply,
+     assign(socket, show_iata_from_autocomplete: true, show_iata_to_autocomplete: false)}
+  end
+
+  def handle_event("show_iata_to_autocomplete", _params, socket) do
+    {:noreply,
+     assign(socket, show_iata_to_autocomplete: true, show_iata_from_autocomplete: false)}
+  end
 
   def handle_event("save", %{"calculator" => calculator_params}, socket) do
     case Calculators.create_calculator(calculator_params) do
@@ -45,5 +86,30 @@ defmodule Co2OffsetWeb.CalculatorLive.New do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  defp get_autocomplete_records(token) when byte_size(token) <= 2 do
+    %{}
+  end
+
+  defp get_autocomplete_records(search_term) do
+    term = String.downcase(search_term)
+
+    term
+    |> query()
+    |> Repo.all()
+    |> Enum.group_by(fn a -> a.city end)
+  end
+
+  defp query(term) do
+    term = "#{term}%"
+
+    from a in Airport,
+      where:
+        like(fragment("lower(?)", a.city), ^term) or
+          like(fragment("lower(?)", a.country), ^term) or
+          like(fragment("lower(?)", a.name), ^term) or
+          like(fragment("lower(?)", a.iata), ^term),
+      limit: 10
   end
 end
